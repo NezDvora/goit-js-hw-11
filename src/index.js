a2137acdac37eed83f95d72f65d79c9d221c16ae
NezDvora <122118918+NezDvora@users.noreply.github.com>
2023-06-15 01:07:09 +0300

    fix problem with repository

--- src/index.js
+++ src/index.js
@@ -11,19 +11,20 @@ const marker = document.querySelector('.marker');
 const options = {
 rootMargin: '0px',
 threshold: 1
-};
+}
 
 const observer = new IntersectionObserver(observerCallback, options);
-let lightbox;
 
-function initLightbox() {
-lightbox = new SimpleLightbox('.gallery div a', {
+let lightbox = new SimpleLightbox('.gallery div a', {
 captionsData: 'alt',
 captionPosition: 'bottom',
 captionDelay: 250,
-scrollZoom: false
+scrollZoom: false,
 });
-}
+
+let page = 0;
+
+searchForm.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
 event.preventDefault();
@@ -33,25 +34,25 @@ observer.unobserve(marker);
 const value = searchInput.value.trim();
 if (value !== '') {
 fetchQuery(value, page)
-    .then(response => {
-    if (response.data.totalHits > 0) {
-        Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
-        gallery.insertAdjacentHTML('beforeend', markup(response));
-        lightbox.refresh();
-    } else {
-        Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
-        );
-    }
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
+.then(response => {
+if (response.data.totalHits > 0) {
+    Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+    gallery.insertAdjacentHTML('beforeend', markup(response));
     lightbox.refresh();
-    observer.observe(marker);
-    });
+} else {
+    Notify.warning(
+    'Sorry, there are no images matching your search query. Please try again.'
+    );
+}
+})
+.catch(error => console.log(error))
+.finally(() => {
+lightbox.refresh();
+observer.observe(marker);
+});
 } else {
 Notify.info(
-    `It seems you didn't write anything, please specify what exactly you are looking for.`
+`It seems you didn't write enything, please specify what exactly you are looking for`
 );
 }
 }
@@ -60,75 +61,73 @@ function markup(obj) {
 const arrHits = obj.data.hits;
 return arrHits
 .map(
-    ({
-    webformatURL,
-    largeImageURL,
-    tags,
-    likes,
-    views,
-    comments,
-    downloads
-    }) => {
-    lightbox.refresh();
-    return `
-        <div class="photo-card">
-        <div class="photo-wrap">
-            <a href="${largeImageURL}">
-            <img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>
-        </div>
-        <div class="info">
-            <p class="info-item">
-            <b>Likes</b>:</br>${likes}
-            </p>
-            <p class="info-item">
-            <b>Views</b>:</br>${views}
-            </p>
-            <p class="info-item">
-            <b>Comments</b>:</br>${comments}
-            </p>
-            <p class="info-item">
-            <b>Downloads</b>:</br>${downloads}
-            </p>
-        </div>
-        </div>
+({
+webformatURL,
+largeImageURL,
+tags,
+likes,
+views,
+comments,
+downloads,
+}) => {
+lightbox.refresh();
+return `
+<div class="photo-card">
+<div class="photo-wrap">
+    <a href="${largeImageURL}">
+    <img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy" />  
+    </a>
+</div>
+<div class="info">
+    <p class="info-item">
+        <b>Likes</b>:</br>${likes}
+    </p>
+    <p class="info-item">
+        <b>Views</b>:</br>${views}
+    </p>
+    <p class="info-item">
+        <b>Comments</b>:</br>${comments}
+    </p>
+    <p class="info-item">
+        <b>Downloads</b>:</br>${downloads}
+    </p>
+</div>
+</div>
     `;
-    }
+}
 )
 .join('');
 }
 
 function smoothScroll(gallery, step) {
-const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+const { height: cardHeight } =
+gallery.firstElementChild.getBoundingClientRect();
 window.scrollBy({
 top: cardHeight * step,
-behavior: 'smooth'
+behavior: 'smooth',
 });
 }
 
-function observerCallback(entries) {
+function observerCallback(entries){
 entries.forEach(entry => {
 if (entry.isIntersecting === true) {
-    page += 1;
-    fetchQuery(searchInput.value, page)
-    .then(response => {
-        const lastPage = Math.ceil(response.data.totalHits / PER_PAGE);
-        if (page === lastPage) {
+page +=1;
+fetchQuery(searchInput.value, page)
+.then(response => {
+    const lastPage = Math.ceil(response.data.totalHits / PER_PAGE);
+    if (page === lastPage){
         observer.unobserve(marker);
-        Notify.info(`We're sorry, but you've reached the end of search results.`);
-        }
-        if (response.data.totalHits !== 0) {
-        gallery.insertAdjacentHTML('beforeend', markup(response));
-        smoothScroll(gallery, 2);
-        }
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-        lightbox.refresh();
-    });
-}
+    Notify.info(`We're sorry, but you've reached the end of search results.`);            
+    }
+    if (response.data.totalHits !== 0){
+    gallery.insertAdjacentHTML('beforeend', markup(response));
+    smoothScroll(gallery, 2);
+    }
+})
+.catch(error => console.log(error))
+.finally(() => {
+lightbox.refresh();
 });
 }
-
-searchForm.addEventListener('submit', onSubmit);
-initLightbox();
+})
+};
